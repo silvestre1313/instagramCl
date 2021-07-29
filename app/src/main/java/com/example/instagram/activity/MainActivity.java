bc6e2feed8f813125ba1f6d66946b336152ab38f
Patrick <patrick.silvestre13@hotmail.com>
2021-07-29 20:01:20 -0300

    Configurando navigation bottom

--- app/src/main/java/com/example/instagram/activity/MainActivity.java
+++ app/src/main/java/com/example/instagram/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.instagram.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,8 +13,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.instagram.R;
+import com.example.instagram.fragment.FeedFragment;
+import com.example.instagram.fragment.PerfilFragment;
+import com.example.instagram.fragment.PesquisaFragment;
+import com.example.instagram.fragment.PostagemFragment;
 import com.example.instagram.helper.ConfiguracaoFirebase;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +39,57 @@ public class MainActivity extends AppCompatActivity {
         //Configuração de obejtos
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
+        //Configurar bottom navigation view
+        configurarBottomNavigationView();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+
+    }
+
+    //Metodo responsavel por criar a bottom navigation
+    private void configurarBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+
+        //Faz configurações iniciais do bottom navigation
+
+
+        //Habilitar navegação
+        habilitarNavegacao(bottomNavigationViewEx);
+
+        //Configura item selecionado inicialmente
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+    }
+
+    //Metodo responsavel por tratar eventos de clique na bottom navigation
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(item -> {
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            switch (item.getItemId()){
+                case R.id.ic_home:
+                    fragmentTransaction.replace(R.id.viewPager, new FeedFragment()).commit();
+                    return true;
+                case R.id.ic_pesquisa:
+                    fragmentTransaction.replace(R.id.viewPager, new PesquisaFragment()).commit();
+                    return true;
+                case R.id.ic_postagem:
+                    fragmentTransaction.replace(R.id.viewPager, new PostagemFragment()).commit();
+                    return true;
+                case R.id.ic_perfil:
+                    fragmentTransaction.replace(R.id.viewPager, new PerfilFragment()).commit();
+                    return true;
+            }
+
+            return false;
+        });
     }
 
     @Override
