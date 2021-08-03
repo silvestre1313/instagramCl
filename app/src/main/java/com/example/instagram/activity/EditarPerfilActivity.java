c4f302365ba4975a60a3081195c8b922a0152a7a
Patrick <patrick.silvestre13@hotmail.com>
2021-08-03 20:49:32 -0300

    Criando tela de alteração de dados

---
+++ app/src/main/java/com/example/instagram/activity/EditarPerfilActivity.java
@@ -0,0 +1,26 @@
+package com.example.instagram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.instagram.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+
+        //Configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Editar perfil");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black);
+
+    }
+}
