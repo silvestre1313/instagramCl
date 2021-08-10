f874a70c314a23f8d7b782e2bfadc922504d6ad5
Patrick <patrick.silvestre13@hotmail.com>
2021-08-09 23:03:47 -0300

    Criando tela de pesquisa

--- app/src/main/java/com/example/instagram/fragment/PesquisaFragment.java
+++ app/src/main/java/com/example/instagram/fragment/PesquisaFragment.java
@@ -3,64 +3,47 @@ package com.example.instagram.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.instagram.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PesquisaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PesquisaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private SearchView searchViewPesquisa;
+    private RecyclerView recyclerPesquisa;
 
     public PesquisaFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PesquisaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PesquisaFragment newInstance(String param1, String param2) {
-        PesquisaFragment fragment = new PesquisaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pesquisa, container, false);
+        View view = inflater.inflate(R.layout.fragment_pesquisa, container, false);
+
+        searchViewPesquisa = view.findViewById(R.id.searchViewPesquisa);
+        recyclerPesquisa   = view.findViewById(R.id.recyclerPesquisa);
+
+        //Configurar searchView
+        searchViewPesquisa.setQueryHint("Buscar usuarios");
+        searchViewPesquisa.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return view;
     }
 }
