38dbc3735d8b36e1981eca6cd79b1668379c58b7
Patrick <patrick.silvestre13@hotmail.com>
2021-10-28 20:27:08 -0300

    Recuperando comentarios

--- app/src/main/java/com/example/instagram/activity/ComentariosActivity.java
+++ app/src/main/java/com/example/instagram/activity/ComentariosActivity.java
@@ -1,7 +1,10 @@
 package com.example.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +12,31 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.instagram.R;
+import com.example.instagram.adapter.AdapterComentario;
+import com.example.instagram.helper.ConfiguracaoFirebase;
 import com.example.instagram.helper.UsuarioFirebase;
 import com.example.instagram.model.Comentario;
 import com.example.instagram.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComentariosActivity extends AppCompatActivity {
 
     private EditText editComentario;
     private String idPostagem;
     private Usuario usuario;
+    private RecyclerView recyclerComentarios;
+    private AdapterComentario adapterComentario;
+    private List<Comentario> listaComentarios = new ArrayList<>();
+
+    private DatabaseReference firebaseRef;
+    private DatabaseReference comentariosRef;
+    private ValueEventListener valueEventListenerComentarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +45,11 @@ public class ComentariosActivity extends AppCompatActivity {
 
         //Inicializar componentes
         editComentario = findViewById(R.id.editComentario);
+        recyclerComentarios = findViewById(R.id.recyclerComentarios);
 
         //Configuraçoes iniciais
         usuario = UsuarioFirebase.getDadosUsuarioLogado();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
 
         //Configura toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -38,11 +59,51 @@ public class ComentariosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black);
 
+        //Configura recyclerView
+        adapterComentario = new AdapterComentario(listaComentarios, getApplicationContext());
+        recyclerComentarios.setHasFixedSize(true);
+        recyclerComentarios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerComentarios.setAdapter(adapterComentario);
+
         //Recupera id da postagem
         Bundle bundle = getIntent().getExtras();
         if (bundle !=null){
             idPostagem = bundle.getString("idPostagem");
         }
+    }
+
+    private void recuperarComentarios(){
+
+        comentariosRef = firebaseRef.child("comentarios")
+                .child(idPostagem);
+        valueEventListenerComentarios = comentariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listaComentarios.clear();
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    listaComentarios.add(ds.getValue(Comentario.class));
+                }
+                adapterComentario.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarComentarios();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        comentariosRef.removeEventListener(valueEventListenerComentarios);
     }
 
     public void salvarComentario(View view){
